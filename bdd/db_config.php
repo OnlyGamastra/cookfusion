f2a33f4fb59d73af99f2bf713fe91618e73f7619
OnlyGamastra <79911985+OnlyGamastra@users.noreply.github.com>
2023-07-10 19:24:53 +0200

    Update db_config.php

--- bdd/db_config.php
+++ bdd/db_config.php
@@ -1,9 +1,10 @@
 <?php
 
 // Configuration de la base de données
-$db_host = 'localhost'; // Adresse du serveur de base de données
+$db_host = 'srvcookfusion.mysql.database.azure.com'; // Adresse du serveur de base de données
 $db_name = 'cookfusion'; // Nom de la base de données (à modifier si nécessaire)
-$db_user = 'root'; // Nom d'utilisateur de la base de données
-$db_password = 'root'; // Mot de passe de la base de données
+$db_user = 'cookfusion_adm'; // Nom d'utilisateur de la base de données
+$db_password = 'isuOjT2VqO94X0w8vm8G'; // Mot de passe de la base de données
+$ssl-mode = 'require'; // Forcer l'utilisation du ssl
 
 ?>
